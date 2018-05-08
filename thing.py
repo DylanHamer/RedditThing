5833c748882eff18b09b281994eb47479bdcd0ff
Dylan Hamer <dylanhamer13@gmail.com>
2018-05-08 13:17:56 +0100

    Initial commit

---
+++ thing.py
@@ -0,0 +1,39 @@
+"""
+SimpleNews
+Get interesting things from Reddit
+By Dylan Hamer
+"""
+
+import praw
+
+client = "Lzf9MWt6VKQbGg"
+secret = "UaEoRGae6DWmoBz7uUhp4CSeTxY"
+agent = "SimpleNews (by /u/dylanhamer13)"
+
+reddit = praw.Reddit(client_id=client,
+                     client_secret=secret,
+                     user_agent=agent)
+
+def getStuff(post=4):
+    limit = post+2
+    jokes = reddit.subreddit("jokes").hot(limit=limit)
+    showerthoughts = reddit.subreddit("showerthoughts").hot(limit=limit)
+    news = reddit.subreddit("news").hot(limit=limit)
+    
+    joke = list(jokes)[post]
+    jokeTitle = joke.title
+        jokeBody = joke.body
+
+    showerthought = list(showerthoughts)[post].title
+    newsHeadline = list(news)[post].title
+    
+    return jokeBody, jokeTitle, showerthought, newsHeadline
+    
+def main():
+    jokeBody, jokeTitle, showerthought, newsHeadline = getStuff()
+    print("Hello, Dylan!")
+    print("Joke Of The Day: {} \n {}".format(jokeTitle, jokeBody))
+    print("Showerthought Of The Day: {}".format(showerthought))
+    print("News Of The Day: {}".format(newsHeadline))
+    
+main()
